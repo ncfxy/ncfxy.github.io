fdc6812e6d1f90e3ed343f4d21f5f52cc5c841ae
ncfxy <359419163@qq.com>
2019-01-25 20:04:06 +0800

    feat(personalIncomeTax): add personal income tax page

---
+++ js/personal_income_tax_2019.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#main-app',
+    data: {
+        incomeBeforeTax: 10000,
+        fiveInsuranceOneFund: 3000,
+        specialItems: 2000
+    },
+    methods: {
+        startCalc: function(){
+            
+        }
+    }
+});
